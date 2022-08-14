14f7fa8cd7ea84292f7b4c67c0a1cccdda3ad8aa
Georgi Petrov <hailanimepenchou@gmail.com>
2022-08-14 14:07:52 +0300

    init

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import { Footer } from './components/shared/footer/Footer';
+import { Header } from './components/shared/header/Header';
+
+function App() {
+    return (
+        <div>
+            <Header />
+
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
---
+++ client/src/components/shared/footer/Footer.js
@@ -0,0 +1,7 @@
+export const Footer = (params) => {
+    return (
+        <footer className="footer">
+            <p>Copyright © designed by Mihail Valkov</p>
+        </footer>
+    );
+};
